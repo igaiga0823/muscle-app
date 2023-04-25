c0f18f4978fbe3f2d1bd96ac4e5ea96255037acf
igaiga0823 <93360615+igaiga0823@users.noreply.github.com>
2023-04-25 23:29:28 +0900

    ハッシュ化

--- backend/Signin.py
+++ backend/Signin.py
@@ -8,8 +8,7 @@ import json
 import sqlite3
 from collections import defaultdict
 import sys
-
-
+import random
 
 conn = MySQLdb.connect(
 host = 'mysql213.phy.lolipop.lan',
@@ -19,21 +18,38 @@ db = 'LAA1475865-muscle',
 charset = 'utf8')
 cur = conn.cursor()
 
-def signin(user_id,after_password):
-    sql = "select PASSWORD,NICK_NAME from USER where USER_ID =%s;"
-    cur.execute(sql,(user_id,))
+
+
+def signin(user_name, after_password, session_id):
+    sql = "select PASSWORD, USER_NAME,EMAIL_ADDRESS,USER_ID from USER where USER_NAME =%s;"
+    cur.execute(sql,(user_name,))
     data = cur.fetchall()
+    
+
     if len(data)==0:#もしユーザーが存在しない
         return "{"+"loginStatus"+":"+"Null"+"}"
-    password=data[0][0]
-    nick_name=data[0][1]
- 
-    #ここでrandomの変数をrand
-    #passward hashか
-    if after_password==password:
-        return "{"+"loginStatus"+":"+"True"+"}"
-    else:
-        return "{"+"loginStatus"+":"+"False"+"}"
+    for i in range(len(data)):
+        originPassword = data[i][0]
+        user_name = data[i][1]
+        user_id = data[i][3]
+        #ここでrandomの変数をrand
+        #passward hashか
+        if after_password == originPassword:
+            ans = { "loginStatus" : "True", "userId" : user_id, "userName" : user_name }
+
+            conn.commit()
+
+            cur.close()
+            conn.close()
+            return ans
+
+    ans = { "loginStatus" : "False" }
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+    return ans
     
         #return "{"+"loginStatus"+":"+"False"+"}"
 
@@ -41,5 +57,5 @@ def signin(user_id,after_password):
 #    cur.execute(sql3, (user_id, ))
 
 if __name__ == "__main__": 
-    signin(1,"daidai")
+    signin("test","b59c67bf196a4758191e42f76670ceba",11)
     
