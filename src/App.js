233f8277cd514e82d164b08a2b629002b142c6cd
igaiga0823 <93360615+igaiga0823@users.noreply.github.com>
2023-05-02 13:02:43 +0900

    グラフ完成

--- src/App.js
+++ src/App.js
@@ -4,30 +4,29 @@ import './css/reset.css'
 import { BrowserRouter, Route, Routes, Link} from 'react-router-dom';
 import React, { useState, useEffect } from "react";
 
-
+import None from './components/pages/None';
 
 import Fetch1 from './Fetch1';
-//import LoginForm1 from './components/LoginForm1';
-import Home from './components/Home';
 import Test from './components/test';
 
-import Login from './components/views/Login';
+import Login from './components/views/Login'; 
 import AppBar from './components/parts/AppBar';
-import AppBar1 from './components/parts/AppBar1';
+import Graph from './components/views/Graph';
 
 function App() {
   return (
     <div className="App">
 
-
+      {/* <AppBar /> */}
       <BrowserRouter>
         <Routes> 
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<None />} />
           {/* <Route path="/login1" element={<LoginForm1 />} /> */}
           <Route path="/test" element={<Test />} />
           <Route path="/fetch" element={<Fetch1 />} />
           <Route path="/loginform" element={<Login />} />
-          <Route path="/appbar1" element={<AppBar1 />} />
+          <Route path="/appbar" element={<AppBar />} />
+          <Route path="/graph" element={<Graph />} />
         </Routes>
       </BrowserRouter>
 
