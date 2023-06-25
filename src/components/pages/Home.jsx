dcee15637361084fb67fbc27f15927ec3e43020d
MiharuNamiki <miharu.namiki.5x@gmail.com>
2023-06-25 14:44:15 +0900

    Gridデザインのテスト

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'css/Home.css'
-import { Box, TextField, Autocomplete, Button, Alert } from "@mui/material";
+import { Box, TextField, Autocomplete, Button, Alert, Grid } from "@mui/material";
 
 const Home = () => {
   return (
@@ -8,23 +8,30 @@ const Home = () => {
       <div className="bg"></div>
       <div className="bg bg2"></div>
       <div className="bg bg3"></div>
-      <Box
-        sx={{
-          backgroundColor: 'rgba(75, 0, 130, 0.8)',
-          borderRadius: '1.25em',
-          boxShadow: '0 0 .5em rgba(0, 0, 0, .5)',
-          boxSizing: 'border-box',
-          left: '50%',
-          padding: '6vmin',
-          position: 'fixed',
-          textAlign: 'center',
-          top: '48%',
-          transform: 'translate(-50%, -50%)',
-        }}
-      >
-        <h1 style={{ color: '#F8F8FF' }} >ようこそ Muscle Appへ!</h1>
-      </Box>
-    </div>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Box
+
+          >
+            <h1 >ようこそ Muscle Appへ!</h1>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box
+
+          >
+            <h1 >ようこそ Muscle Appへ!</h1>
+          </Box>
+        </Grid>
+        <Grid item xs={4}>
+
+        </Grid>
+        <Grid item xs={8}>
+
+        </Grid>
+      </Grid>
+
+    </div >
   );
 };
 
